d006b503fa51c8ca827c62c5a2ab0c883d4bf5fc
Renan Passos <68907028+renanvpassos@users.noreply.github.com>
2026-08-06 13:27:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ PLANILHAS_URLS = [
     "https://docs.google.com/spreadsheets/d/1uJzArQ8oF19s2yYQD3BFoNeaZW_xPMdD1RvdSIWnGR8/edit?usp=sharing", #VALERIA
     "https://docs.google.com/spreadsheets/d/1Q0BMTebNMSEyGqTwuQjy2r6nLeSNQE7oIhEntpUhQAA/edit?gid=0#gid=0", #SALVADOR LENNON
     "https://docs.google.com/spreadsheets/d/10P8YgNIqxox-MqDA63DnO5yKAueAQ5GgJONDH2fu9-8/edit?gid=0#gid=0", #RIO LENNON
+    "https://docs.google.com/spreadsheets/d/1gNeE9CY8KLaI7DOajWFJcGmZ-UuS4ME8firbFkovNS4/edit?gid=0#gid=0", #ABB
 ]
 
 def normalizar_texto(texto):
