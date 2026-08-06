63d194cacbb5ec0a7d57e1b24d4ccb9245c68dbe
Renan Passos <68907028+renanvpassos@users.noreply.github.com>
2026-08-06 12:06:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ except Exception:
 
 PLANILHAS_URLS = [
     "https://docs.google.com/spreadsheets/d/1ym-kHhuaW1pD5KNXzrmgY2QaUSol339R4fCHdGRS3K8/edit?usp=sharing",
+    "https://docs.google.com/spreadsheets/d/1zRkVSttkkpqekEdXjGPlz3-Dl7NzgqnkbGioJGuAdRY/edit?usp=sharing",
 ]
 
 def normalizar_texto(texto):
